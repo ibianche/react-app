230e61d0e4a2829d0748a6a60a55abeb6445eee6
ibianche <inna.pomorska@gmail.com>
2022-07-27 14:22:26 +0200

    LifeCycle s2f14

--- src/components/second.js
+++ src/components/second.js
@@ -3,8 +3,23 @@ import React from 'react';
 class Second extends React.Component {
 
   state = {
-    name: 'Inna'
+    name: 'Inna',
+    surname:'',
+    data: new Date()
   };
+
+  componentDidMount() {
+    console.log('poczatek komponentu');
+    this.setState({surname: this.props.surname});
+    this.inter = setInterval(() => {
+      this.setState({data: new Date()})
+    }, 1000);
+  };
+
+  componentWillUnmount() {
+    clearInterval(this.inter);
+    console.log('koniec komponentu');
+  }
 
   klik = (evt) => {
     this.setState(   //zmiana state za pomoca funkcji setState
@@ -12,18 +27,19 @@ class Second extends React.Component {
       () => console.log('wew', this.state.name)
     );
     console.log('zew', this.state.name);
-  }
+  };
 
-  zmiana(evt){
-    console.log('input zmiana', evt.target.value);
-  }
+  zmiana = (evt) => {
+    this.setState({surname: evt.target.value});
+  };
 
   render() {
     return(
       <div>
+        <h1>{this.state.data.toString()}</h1>
     <h1>Moje imie to: {this.state.name}</h1>
     <h1 onClick={this.klik}>
-      Moje nazwisko to: {this.props.surname}</h1>
+      Moje nazwisko to: {this.state.surname}</h1>
         <input type='text' onChange={this.zmiana}/>
       </div>
     )
