22088185d2728147ca39d84e7a9c1c0ab8bae5ec
ibianche <inna.pomorska@gmail.com>
2022-07-27 11:05:35 +0200

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 import React from 'react';
 
+
+class Second extends React.Component {
+  render() {
+    return <h1>Moje nazwisko to: {this.props.surname}</h1>
+  }
+}
+
+function First(props) {
+  return <h1>Moje imie to: {props.name}</h1>
+}
+
+
+
+
 function App() {
+
+  let myName = 'Inna';
+  let mySurname = 'Bianche'
   return (
-
-    <p>To jest moj pierwszy element</p>
-
+    <div>
+    <First name={myName}/>
+    <Second surname={mySurname}/>
+    </div>
   );
 }
 
