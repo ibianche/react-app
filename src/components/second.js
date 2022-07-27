7669f818199cee397a5f6dd8aa966af1afbf995f
ibianche <inna.pomorska@gmail.com>
2022-07-27 12:50:56 +0200

    events s2f12

--- src/components/second.js
+++ src/components/second.js
@@ -4,14 +4,23 @@ class Second extends React.Component {
 
   state = {
     name: 'Inna'
+  };
+
+  klik(evt){
+    console.log('kliknalem2', evt);
+  }
+
+  zmiana(evt){
+    console.log('input zmiana', evt.target.value);
   }
 
   render() {
     return(
       <div>
     <h1>Moje imie to: {this.state.name}</h1>
-    <h1 onClick={ (evt) => console.log('kliknalem', evt)}>
+    <h1 onClick={this.klik}>
       Moje nazwisko to: {this.props.surname}</h1>
+        <input type='text' onChange={this.zmiana}/>
       </div>
     )
   }
