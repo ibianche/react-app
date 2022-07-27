1211204271ba7e13bbf42fd8f79ce1211293f45e
ibianche <inna.pomorska@gmail.com>
2022-07-27 12:42:29 +0200

    events

--- src/components/second.js
+++ src/components/second.js
@@ -10,7 +10,8 @@ class Second extends React.Component {
     return(
       <div>
     <h1>Moje imie to: {this.state.name}</h1>
-    <h1>Moje nazwisko to: {this.props.surname}</h1>
+    <h1 onClick={ (evt) => console.log('kliknalem', evt)}>
+      Moje nazwisko to: {this.props.surname}</h1>
       </div>
     )
   }
