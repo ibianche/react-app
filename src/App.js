5cfe8978f7d1df6ce81e672fe88d4a7c8af49d91
ibianche <inna.pomorska@gmail.com>
2022-07-27 16:23:04 +0200

    funkcje warunkowe w app s2f17

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import React from 'react';
 // import Second from './components/second'
 import First from './components/first';
 
+function Zaloguj() {
+  return <h1>Zaloguj się</h1>
+}
+
+function Wyloguj() {
+  return <h1>Wyloguj się</h1>
+}
+
+function Auth(props) {
+  if(props.zalogowany){
+    return <Wyloguj/>
+  }else{
+    return <Zaloguj/>
+  }
+}
+
+
 function App() {
 
   let mySurname = 'Bianche';
+
   return (
     <div>
-      <First surname={mySurname}/>
+      <Auth zalogowany={false}/>
     </div>
   );
 }
