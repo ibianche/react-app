3232be54774f52de43bf0a57888ed796ed9f062a
ibianche <inna.pomorska@gmail.com>
2022-07-28 10:46:33 +0200

    nie dzialajaca wersja linki

--- src/App.js
+++ src/App.js
@@ -1,38 +1,10 @@
-import React, {useState} from 'react';
+import React  from 'react';
 
 
 function App() {
 
-  const [imie, setImie] = useState('');
-  const [wybor, setWybor] = useState('tak');
-
-  const wyslij = (evt) => {
-    evt.preventDefault();
-    console.log('Nasze imie to: ', imie, wybor)
-  };
-
-  const zmianaImie = (evt) => {
-   setImie(evt.target.value);
-  };
-
-  const zmianaWybor = (evt) => {
-    setWybor(evt.target.value);
-  };
-
-
   return (
-    <form onSubmit={wyslij}>
-      <label>
-        Imie:
-        <input type='text' value={imie} onChange={zmianaImie}/>
-      </label>
-      <select value={wybor} onChange={zmianaWybor}>
-        <option value='tak'>Tak</option>
-        <option value='nie'>Nie</option>
-      </select>
-      <br/>
-      <input type='submit' value='wyslij'/>
-    </form>
+   <h1>To jest app comp</h1>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import First from './components/first';
+import Second from './components/second';
+
+ReactDOM.render(
+  <Router>
+    <div>
+      <Route exact path="/">
+        <App />
+      </Route>
+      <Route path="/first">
+        <First />
+      </Route>
+      <Route path="/second">
+        <Second />
+      </Route>
+    </div>
+  </Router>
+);
+
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -11,12 +31,17 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
 
 
+
+
+
+
+
+// import * as serviceWorker from './serviceWorker';
+// ReactDOM.render(<App />, document.getElementById('root'));
+// serviceWorker.unregister();
